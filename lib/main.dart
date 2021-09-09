92a2299ba69d5db2bb725a94aa623272d58cffdf
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-09-09 19:07:43 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:cripto_moedas/configs/app_settings.dart';
+import 'package:cripto_moedas/configs/hive_config.dart';
+import 'package:cripto_moedas/repositories/favoritas_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'meu_aplicativo.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveConfig.start();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AppSettings()),
+        ChangeNotifierProvider(create: (context) => FavoritasRepository()),
+      ],
+      child: const MeuAplicativo(),
+    ),
+  );
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:cripto_moedas/pages/favoritas_page.dart';
+import 'package:cripto_moedas/pages/moedas_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int paginaAtual = 0;
+  late PageController pc;
+
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: paginaAtual);
+  }
+
+  setPaginaAtual(int pagina) {
+    setState(() {
+      paginaAtual = pagina;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pc,
+        children: [
+          MoedasPage(),
+          FavoritasPage(),
+        ],
+        onPageChanged: setPaginaAtual,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: paginaAtual,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'star'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritas'),
+        ],
+        onTap: (pagina) {
+          pc.animateToPage(
+            pagina,
+            duration: Duration(milliseconds: 400),
+            curve: Curves.ease,
+          );
+        },
+      ),
+    );
+  }
+}
